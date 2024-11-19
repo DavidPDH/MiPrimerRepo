8d958366ad9f080a9387919637c5834e232d92d8
Óscar Quirant García <oscar.chenene@gmail.com>
2024-11-19 23:14:00 +0100

    Add files via upload

---
+++ prueba/src/HolaIntelliJ.java
@@ -0,0 +1,6 @@
+public class HolaIntelliJ {
+    public static void main(String[] args) {
+        System.out.println("Hola desde IntelliJ!");
+        System.out.println("Adiós desde IntelliJ");
+    }
+}
